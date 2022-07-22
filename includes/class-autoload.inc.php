dd5fa769af37bcd2d53976ffe379554119045cf4
geekelo <102464578+geekelo@users.noreply.github.com>
2022-07-22 09:20:05 +0100

    Added initial files

---
+++ includes/class-autoload.inc.php
@@ -0,0 +1,13 @@
+<?php
+spl_autoload_register('myAutoLoader');
+
+function myAutoLoader ($className){
+$path = 'classes/';
+$extension = 'class.php';
+$fileName = $path . $className . $extension;
+
+if (!file_exists($fileName)) {
+    return false;
+}
+include_once $path . $className . $extension;
+}
